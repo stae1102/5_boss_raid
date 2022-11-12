a4707eb4605d891602696862d0e7ca2d30fbb973
Seongtae <stae1102@gmail.com>
2022-11-12 19:21:18 +0900

    feat: Create findRaidHistoryByUserId method
    Fixes: #7

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 
 @Injectable()
 export class UserRepository
@@ -22,5 +22,9 @@ export class UserRepository
 
   async create() {
     return await this.user.create({ data: {}, select: { userId: true } });
+  }
+
+  async findRaidHistoriesByUserId(userId: User['userId']) {
+    return await this.raidHistory.findMany({ where: { userId } });
   }
 }
