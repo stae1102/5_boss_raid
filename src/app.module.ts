c42b370d7534254143a511b9e5d7ee17fe0bdee8
Seongtae <stae1102@gmail.com>
2022-11-12 15:19:00 +0900

    feat: Import cache module and redis store config
    Fixes: #8

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { RedisClientOptions } from 'redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisClientConfig } from './config/redis-client.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync<Promise<RedisClientOptions>>(RedisClientConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/redis-client.config.ts
@@ -0,0 +1,20 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
+
+export const RedisClientConfig = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService) => {
+    const store = await redisStore({
+      socket: {
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+      },
+    });
+    return {
+      store: {
+        create: () => store,
+      },
+    };
+  },
+  inject: [ConfigService],
+};
