b8bd83e061c4c70ef95bfd582cb05fc1bd870adb
Seongtae <stae1102@gmail.com>
2022-11-12 15:40:02 +0900

    feat: Generate user resources
    Controller, Module, Service
    
    Fixes: #6

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { RedisClientOptions } from 'redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisClientConfig } from './config/redis-client.config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     CacheModule.registerAsync<Promise<RedisClientOptions>>(RedisClientConfig),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [
---
+++ src/user/user.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../repository/user.repository';
+
+@Injectable()
+export class UserService {
+}
