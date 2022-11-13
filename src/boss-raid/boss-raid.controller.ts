883d228f7792259c503a6449e5a4ed7651465fed
Seongtae <stae1102@gmail.com>
2022-11-13 23:29:12 +0900

    feat: Add enter bossraid endpoint
    Fixes: #10

--- src/boss-raid/boss-raid.controller.ts
+++ src/boss-raid/boss-raid.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { BossRaidService } from './boss-raid.service';
+import { EnterBossRaidDto } from './dto/enter-boss-raid.dto';
 
 @Controller('bossRaid')
 export class BossRaidController {
   constructor(private readonly bossRaidService: BossRaidService) {}
+
+  @Post()
+  async enterBossRaid(@Body() enterBossRaidDto: EnterBossRaidDto) {
+    return this.bossRaidService.enterBossRaid(enterBossRaidDto);
+  }
 }
