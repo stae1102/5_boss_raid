486041313aa09c35620ddb83286ea70791f81e7f
Seongtae <stae1102@gmail.com>
2022-11-14 18:30:45 +0900

    fix: Change the TTL option
    There's some issue between cache-manager and cache-manager-redis-store.
    Finally, I had found a solution except the ttl error.
    This is my solution to resolve the ttl problem.
    
    Fixes: #11

--- src/config/redis-client.config.ts
+++ src/config/redis-client.config.ts
@@ -1,21 +1,29 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { redisStore } from 'cache-manager-redis-store';
+import { firstValueFrom } from 'rxjs';
 
 export const RedisClientConfig = {
   isGlobal: true,
-  imports: [ConfigModule],
-  useFactory: async (configService: ConfigService) => {
+  imports: [ConfigModule, HttpModule],
+  useFactory: async (
+    configService: ConfigService,
+    httpService: HttpService,
+  ) => {
+    const bossRaidData = await firstValueFrom(
+      httpService.get(await configService.get('BOSSRAID_STATIC_DATA')),
+    );
+    const limitTime = bossRaidData.data.bossRaids[0].bossRaidLimitSeconds;
     const store = await redisStore({
       socket: {
         host: configService.get('REDIS_HOST'),
         port: configService.get('REDIS_PORT'),
       },
+      ttl: limitTime,
     });
     return {
-      store: {
-        create: () => store,
-      },
+      store: () => store,
     };
   },
-  inject: [ConfigService],
+  inject: [ConfigService, HttpService],
 };
